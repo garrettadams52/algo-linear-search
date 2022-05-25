fad6e624f727eb514bf98d3fe2ee2daed47f49e6
Garrett Adams <garrett.adams52@gmail.com>
2022-05-25 17:37:25 -0400

    finished python

--- python/linear_search.py
+++ python/linear_search.py
@@ -4,3 +4,9 @@ for number in range(1, 1001):
 
 def linear_search(value_to_find, array_to_search_through):
     # your code here
+
+    for i,x in enumerate(array_to_search_through):
+        if value_to_find == x:
+            return i
+
+    return None
